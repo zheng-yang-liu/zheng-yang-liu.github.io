38d27b50902fc99a2cb1e97003132d6e679458d2
zheng-yang-liu <thezhengyang@qq.com>
2024-08-09 11:20:48 +0800

    优化

---
+++ assets/index-C9pFFb5W.js
@@ -0,0 +1,14 @@
+import{d as l,c as i,i as n,w as o,r as a,b as s,j as t}from"./index-5Jm3zCii.js";const d=`# NPM\r
+npm install xuyan-ui\r
+\r
+# Yarn\r
+yarn add xuyan-ui\r
+\r
+# pnpm\r
+pnpm install xuyan-ui`,p=`import xy from "xuyan-ui"\r
+\r
+//解构\r
+import {Tools} from "xuyan-ui"\r
+//Tools\r
+//AnimationUtils\r
+//DrawGraph,Graph`,f=l({__name:"index",setup(u){const r=[{title:"安装",id:"281b47bcbe00b684c7d11ff255b682de",slot:"install"},{title:"快速上手",id:"b0de01509cdaec5d6a38306b48cf1248",slot:"quickStart",children:[{title:"完整导入",id:"bbfa338e937c98e85cb9ae9253eccdd7",slot:"fullImport"}]}];return(m,_)=>{const e=a("xy-code-preview"),c=a("xy-showcase-page");return s(),i("div",null,[n(c,{introduction:"以下为xuyan-ui组件库API的基本使用方法","page-title":"使用/安装",catalogue:r},{install:o(()=>[n(e,{code:t(d),language:"markdown"},null,8,["code"])]),fullImport:o(()=>[n(e,{code:t(p),language:"JavaScript"},null,8,["code"])]),_:1})])}}});export{f as default};
