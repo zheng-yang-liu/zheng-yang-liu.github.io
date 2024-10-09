3ecbf108b2dfd0ec708e53f4154b7dd6c1b97d9a
zheng-yang-liu <thezhengyang@qq.com>
2024-10-09 15:18:40 +0800

    路由

---
+++ assets/js/index-Nn5S--NA.js
@@ -0,0 +1,12 @@
+import{d as l,c as s,i as n,w as t,r as o,b as c,j as a}from"./index-QW_h9D4a.js";const d=`# NPM\r
+npm install xuyan-ui\r
+\r
+# Yarn\r
+yarn add xuyan-ui\r
+\r
+# pnpm\r
+pnpm install xuyan-ui`,p=`import xy from "xuyan-ui"\r
+//引入图表\r
+import"xuyan-ui/assets/iconFont/iconfont.css"\r
+\r
+app.use(xy);`,x=l({__name:"index",setup(u){const r=[{title:"安装",id:"8bf55d1f9221ed41c6f8d1f0850e4318",slot:"install"},{title:"快速上手",id:"d31ffed75bf08590f750ff0401313435",slot:"quickStart",children:[{title:"完整导入",id:"f6796928d53e64f6afd6ff96654b4c34",slot:"fullImport"}]}];return(f,m)=>{const e=o("xy-code-preview"),i=o("xy-showcase-page");return c(),s("div",null,[n(i,{introduction:"以下为xuyan-ui组件库的基本使用方法","page-title":"使用/安装",catalogue:r},{install:t(()=>[n(e,{code:a(d),language:"markdown"},null,8,["code"])]),fullImport:t(()=>[n(e,{code:a(p),language:"JavaScript"},null,8,["code"])]),_:1})])}}});export{x as default};
---
+++ assets/js/logo-CoCiVNI0.js
@@ -0,0 +1,1 @@
+const g="/zheng-yang-liu.github.io/assets/img/logo-Dm4kz9Nu.png";export{g as _};
